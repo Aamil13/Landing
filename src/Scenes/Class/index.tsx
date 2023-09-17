dbab8662aaaed971d2eb753498d7aed7e5a98402
Aamil13 <aamil.shafi13@gmail.com>
2023-09-17 19:15:22 +0530

    final push v2

--- src/Scenes/Class/index.tsx
+++ src/Scenes/Class/index.tsx
@@ -41,7 +41,7 @@ import { motion } from "framer-motion";
     //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     //   image: image6,
     // },
-  ];
+  // ];
   
   type Props = {
     setSelectedPage: (value: SelectedPage) => void;
